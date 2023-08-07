c71b65867138afe0cff73b72f708e5e17bf07a13
josueCastillejos <84791173+211234@users.noreply.github.com>
2023-08-07 10:12:59 -0600

    Add files via upload

---
+++ product/domain/Product.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Product = void 0;
+class Product {
+    constructor(id, name, email, phone, password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+}
+exports.Product = Product;
